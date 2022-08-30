64d316041510aae1bc3bb8ad27bfbfecfc1fe323
kotili-tec <weh4teyou@gmail.com>
2022-08-30 15:23:33 +0300

    --amend

--- src/controller/opener.js
+++ src/controller/opener.js
@@ -1,7 +1,5 @@
 import { getYear, getMounth, getDay, findLastPdf } from "./controller.js";
 
-const pathKBP = '//asd/kbp/Сайт'; 
-
 export const openKbp = async (pathToKbp) => {
   const year = await getYear(pathToKbp);
   const month = await getMounth(year);
